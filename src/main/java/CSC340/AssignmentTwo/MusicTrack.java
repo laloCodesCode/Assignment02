eed59a6b1e771e83466a29c77206ab2a55e853c0
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-03 18:38:20 -0500

    initial commit

---
+++ src/main/java/CSC340/AssignmentTwo/MusicTrack.java
@@ -0,0 +1,10 @@
+package CSC340.AssignmentTwo;
+
+public class MusicTrack {
+    private String title;
+    private String  artist;
+    private String album;
+    private String link;
+
+    public musicTrack
+}
