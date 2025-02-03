ae72a0f38bbe9f6245d56da1b50d1ae65ec9cfc4
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-03 18:46:18 -0500

    added the object for artist and album

--- src/main/java/CSC340/AssignmentTwo/MusicTrack.java
+++ src/main/java/CSC340/AssignmentTwo/MusicTrack.java
@@ -2,9 +2,46 @@ package CSC340.AssignmentTwo;
 
 public class MusicTrack {
     private String title;
-    private String  artist;
-    private String album;
+    private Artist artist;
+    private Album album;
     private String link;
 
-    public musicTrack
+    public MusicTrack(String title, Artist artist, Album album, String link) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.link = link;
+
+
+    }
+
+
+    public static class Album {
+        private String title;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+
+    }
+
+
+    public static class Artist {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+
+
