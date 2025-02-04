b64621c8d8911f9516a191b8b4df6e621fcd589d
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-04 09:54:39 -0500

    added to the controller class

--- src/main/java/CSC340/AssignmentTwo/RestApiController.java
+++ src/main/java/CSC340/AssignmentTwo/RestApiController.java
@@ -6,5 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
+
+    private static final String DEEZER_API_URL = "https://api.deezer.com/search?q=Bad+Bunny";
+
+    @GetMapping("/badbunny")
+    public MusicTrack[] getBadBunnyTracks() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(DEEZER_API_URL, MusicTrack[].class);
+    }
 }
